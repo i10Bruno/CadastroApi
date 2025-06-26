1423104076965b1e4878af7f08a06ede5f687c5e
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-06-26 11:24:22 -0300

    Fix:sets email column as unique

--- src/main/java/dev/cadastro/CadastroDeClientes/Clientes/controller/service/ClienteModel.java
+++ src/main/java/dev/cadastro/CadastroDeClientes/Clientes/controller/service/ClienteModel.java
@@ -2,59 +2,32 @@ package dev.cadastro.CadastroDeClientes.Clientes.controller.service;
 
 import dev.cadastro.CadastroDeClientes.Tarefas.TarefasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String Nome;
-    private   String Email;
+
+    @Column(unique = true)
+    private String Email;
     private int idade;
     //sprivate List<TarefasModel> tarefas;
-
 
 
     //um cliente so tem uma unica tarefa @ManyToOne
     @ManyToOne()
     @JoinColumn(name = "tarefas_id")
-   private TarefasModel tarefas;
+    private TarefasModel tarefas;
 
-    public ClienteModel(){
-
-    }
-
-    public ClienteModel(String nome, String email, int idade) {
-        Nome = nome;
-        Email = email;
-        this.idade = idade;
-    }
-
-
-    public String getNome() {
-        return Nome;
-    }
-
-    public void setNome(String nome) {
-        Nome = nome;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
