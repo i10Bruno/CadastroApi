a08be7ad37aa7cbbdc259692124238ee59d74efe
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-07-02 10:23:58 -0300

    feat: create initial controllers for tarefas

--- src/main/java/dev/cadastro/CadastroDeClientes/Tarefas/TarefasController.java
+++ src/main/java/dev/cadastro/CadastroDeClientes/Tarefas/TarefasController.java
@@ -1,4 +1,34 @@
 package dev.cadastro.CadastroDeClientes.Tarefas;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("tarefas")
+
 public class TarefasController {
+
+    @PostMapping("/criar")
+    public String tarefaCriada() {
+
+
+        return "tarefacriada";
+    }
+
+    @PutMapping("/alterar")
+    public String tarefaalterada() {
+
+
+        return "alterado";
+
+    }
+    @DeleteMapping("/deletar")
+    public String deletar() {
+
+
+        return "deletado com sucesso";
+
+    }
+
+
 }
